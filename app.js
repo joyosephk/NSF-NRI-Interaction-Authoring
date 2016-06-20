10155d2ee83918cd4509cd325d51a65321150ed8
Josh McGrath <joshuawmcgrath@gmail.com>
2016-06-20 16:09:12 -0500

    object action menu, object creator backend, proper object highlighting

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var app = (function(){
 	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
 	var cube = new THREE.Mesh( geometry, material );
-	//cube.translate(1,1,1);
 	cube.boundingSphere = 1;
 	var axisHelper = new THREE.AxisHelper(20);
 	var tableGeom = new THREE.BoxGeometry(2,1,20);
@@ -75,17 +74,45 @@ var app = (function(){
 		document.body.appendChild(div);
 
 	}
+	var display = function(options){
+		menu = document.getElementById('menu');
+		while (menu.firstChild) {
+			    menu.removeChild(menu.firstChild);
+		}
+		for(i in options){
+			text = options[i];
+			var div = document.createElement('div');
+			div.innerHTML = text;
+			div.className = "btn btn-option";
+			menu.appendChild(div);
+		}	
+	}
+	var rightClickHandler = function(event){
+		findIntersect(function(object){
+			if(object.options){
+				display(object.options);
+			}
+		});	
+	}
 	var selected_object = null;
 	document.addEventListener( 'mousemove', mouseEvent, false );
 	var clickHandler = function(event){
+		mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+		mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;		
+
+		if(event.ctrlKey || event.which == 3){
+			rightClickHandler(event);
+			return;
+		}
 		if(selected_object){
 			placeCopy(event);
 			//need to place object
 			return;
 		}
-		mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-		mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;		
-		findIntersect(function(object){
+			findIntersect(function(object){
+			if(object.isSurface){
+				return;
+			}
 			object.material.color = new THREE.Color(1,1,1);
 			object.lock = true;
 			makeDiv(event, 1);
@@ -95,7 +122,7 @@ var app = (function(){
 	}
 	
 	
-	document.addEventListener('click', clickHandler,true);
+	document.addEventListener('mousedown', clickHandler,true);
 	var findIntersect = function(operation){
 		raycaster.setFromCamera(mouse,camera);
 		var intersects = raycaster.intersectObjects( scene.children );
@@ -122,6 +149,13 @@ var app = (function(){
 
 	var cameraTheta = 0;
  	var cameraRadius = 15;
+	var cameraChange = function(){
+		camera.position.x = Math.cos(cameraTheta)*cameraRadius*Math.cos(0);
+		camera.position.z = Math.sin(cameraTheta)*cameraRadius*Math.sin(Math.PI/2);
+	//	camera.position.z = Math.cos(cameraTheta)*cameraRadius;	
+		camera.lookAt(zeroVec);
+
+	}
 	var keyDownHandler = function(event){
 		key = event.code;
 		console.log(event.code)
@@ -134,23 +168,64 @@ var app = (function(){
 				break;
 
 			case 'ArrowUp':
-				cameraPhi += .14;
+		//		cameraPhi += .14;
 				break;
 			case 'ArrowDown':
-				cameraPhi += -.14;
+		//		cameraPhi += -.14;
 				break;
 		}
-		camera.position.x = Math.cos(cameraTheta)*cameraRadius*Math.cos(0);
-		camera.position.z = Math.sin(cameraTheta)*cameraRadius*Math.sin(Math.PI/2);
-	//	camera.position.z = Math.cos(cameraTheta)*cameraRadius;	
-		camera.lookAt(zeroVec);
+		cameraChange();
 	}
+	var left = document.querySelector('#camera-left');
+	left.addEventListener('click',function(){
+		cameraTheta += .14;
+		cameraChange();
+	},false)
+	var right = document.querySelector('#camera-right');
+	right.addEventListener('click', function(){
+		cameraTheta += -.14;
+		cameraChange();
+	},false)
 	document.addEventListener('keydown',keyDownHandler,false);
+	var objectCreator = function(){
+		var matcher = {
+			'box': THREE.BoxGeometry,
+			'spehere': THREE.SphereGeometry
+		}
+		var createObject = function(choice,options){
+			var geom = new THREE.SphereGeometry(1,8,8);
+			var material = new THREE.MeshBasicMaterial({wireframe:true});
+			var shape = new THREE.Mesh(geom, material);
+			if(options.isSurface){
+				shape.isSurface = true;
+			}
+			shape.position.x = options.pos.x;
+			shape.position.y = options.pos.y;
+			shape.position.z = options.pos.z;
+			shape.options = options.options;
+			scene.add(shape);
+		}
+		return{
+			createObject
+		}	
+	}
+	var creator = new objectCreator();
+	creator.createObject('sphere',{isSurface: false, pos:{x:3,y:1,z:1}, options:['grab','pickup','action_1', 'action_2','define new action']})
+	
 	//render loop
 	function render() {
-		if(!cube.lock){
-			cube.material.color = new THREE.Color(0,0,1);
+		
+		for (i in scene.children){
+			if (scene.children[i].type != 'Mesh'){
+				continue;
+			}
+			if(!scene.children[i].lock){
+				scene.children[i].material.color.set(0x0000ff);
+			}
 		}
+		//if(.lock){
+		//	cube.material.color = new THREE.Color(0,0,1);
+		//}
 		//call all relevant animation code here
 		findIntersect(function(object){
 			object.material.color.set(0xff00ff);
