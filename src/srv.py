391f10ffe96b5047faf2047e7865c76a611c17a9
Marshall Mueller <mnmueller@wisc.edu>
2016-07-29 15:21:09 -0500

    Sort of working version of taskplan creation

--- src/srv.py
+++ src/srv.py
@@ -11,28 +11,44 @@ import rospy
 from moveit_commander import RobotCommander, os, PlanningSceneInterface, roscpp_initialize, roscpp_shutdown
 import sys
 import random
-from functools import wraps
+from functools import wraps, partial
+import logging
+from flask_cors import CORS, cross_origin
 
 app = flask.Flask(__name__)
-
+CORS(app)
 fc = forceControlWrapper.ForceControl()
 
-def decorator(func):
+def logged(func): #, level=logging.INFO, name=None, message=None):
+    '''
+    if func is None:
+        return partial(logged, level=level, name=name, message=message)
+
+
+    # get function arguments name
+    argnames = func.func_code.co_varnames[:func.func_code.co_argcount]
+
+    # get function name
+    fname = name if name else func.__name__
+    logger = logging.getLogger(fname)
+    logmsg = message if message else None
+    '''
     @wraps(func)
-    def func_wrapper(*args):
-        resp = flask.Response(func(*args))
+    def func_wrapper(*args, **kwargs):
+        resp = flask.Response(func(*args, **kwargs))
         resp.headers['Access-Control-Allow-Origin'] = '*'
+        resp.headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS'
         return resp
     return func_wrapper
 
 @app.route("/")
-@decorator
+@logged
 def index(arg=None):
     return flask.render_template("index.html")
 
 models_path = "static/models"
 @app.route("/models/list")
-@decorator
+@logged
 def listModels(arg=None):
     models = []
     for entry in os.listdir(models_path):
@@ -41,7 +57,7 @@ def listModels(arg=None):
     return flask.json.dumps(models)
 
 @app.route("/models/get/<name>")
-@decorator
+@logged
 def getModelInfo(name):
     #returns the path of the model's JSON file, in
     #the future it will also return what type of file it is
@@ -51,20 +67,20 @@ def getModelInfo(name):
 # get all stored positions, ALSO ***NEW*** added lists of paths for each position that they have a planned path to
 # as key 'paths'
 @app.route("/positions/get")
-@decorator
+@logged
 def getPositions(arg=None):
     positions = pGraph.getAuthoringInfo()
     return flask.json.dumps(positions)
 
 # get all overarching plans made in the authoring environment
 @app.route("/plans/get")
-@decorator
+@logged
 def getPlans(arg=None):
     return flask.json.dumps(pGraph.getAuthoredPlans())
 
 # save new position
 @app.route("/positions/save/<name>")
-@decorator
+@logged
 def putPosition(name):
     # generate a random ID
     ID = int(random.random() * 1000000)
@@ -76,7 +92,7 @@ def putPosition(name):
 
 # move arm
 @app.route("/positions/move/<ID>")
-@decorator
+@logged
 def putArmGo(ID):
     print "Got and ID to move to" + str(ID)
     try:
@@ -88,8 +104,9 @@ def putArmGo(ID):
 
 # make plan from a dictionary of plans, with each key corresponding to and ID, and the value being a grasp value 0-100
 # 100 = closed, 0 = open, floating point value
-@app.route("/plans/make/<taskname>")
-@decorator
+@app.route("/plans/make/<taskname>", methods=["GET", "POST"])
+@cross_origin()
+@logged
 def putTaskPlan(taskname):
     # path is the dictionary of ID's
     path = flask.request.json['path']
@@ -97,10 +114,13 @@ def putTaskPlan(taskname):
     iterable = iter(path)
     next(iterable)
     # set first node to be current node
-    pGraph.setCurrNode(path[0], acHan)
+    ID = path[0]['id']
+    pGraph.setCurrNode(int(ID), acHan)
     for pose in iterable:
+        print pose
         try:
-            (ID, graspVal) = pose.items()
+            ID = pose['id']
+            print "ID: " + str(ID)
             pGraph.makePath(int(ID), acHan)
         except ValueError:
             print "Non integer-convertible value given for ID"
@@ -110,7 +130,7 @@ def putTaskPlan(taskname):
 
 # plan from current position to another position
 @app.route("/plans/individual/<ID>")
-@decorator
+@logged
 def putPlan(ID):
     try:
         pGraph.makePath(int(ID), acHan)
@@ -121,7 +141,7 @@ def putPlan(ID):
 
 # move to a node from the current node of the arm
 @app.route("/plans/move/<ID>")
-@decorator
+@logged
 def putArmMove(ID):
     try:
         pGraph.moveTo(int(ID), acHan)
@@ -132,7 +152,7 @@ def putArmMove(ID):
 
 # turn on/off force control
 @app.route("/forcecontrol/<on>")
-@decorator
+@logged
 def putForceControl(on):
     if on == "true":
         fc.startForceControl()
@@ -142,7 +162,7 @@ def putForceControl(on):
 
 #grasp
 @app.route("/grasp/<value>")
-@decorator
+@logged
 def grasp(value):
     try:
         value = float(value)
